b7fd0d393fae83a0e35b86eac6931c9a6ecd88ca
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2024-10-08 12:15:26 +0100

    build: Add linting and pre-commit hook

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -25,11 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
   );
 }
