277b424aa56f6238eabb2f7ac280ae483ebcd396
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2024-10-10 22:43:24 +0100

    feat: Add shadcn library and button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -95,6 +97,7 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
+        <Button>Powered by Vercel</Button>
       </footer>
     </div>
   );
