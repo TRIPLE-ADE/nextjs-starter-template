cab7aec40a6fee1db7b4e54df194bb81077d4be3
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2024-10-27 07:21:04 +0100

    test: Update Home component test to use absolute import path

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,13 +1,15 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 
-import Home from "../app/page";
+import Home from "@/app/page";
 
 describe("Home", () => {
   it("renders a heading", () => {
     render(<Home />);
-    const heading = screen.getByRole("heading");
+    const heading = screen.getByRole("heading", {
+      name: "Welcome to Your Next.js Starter Template",
+    });
 
-    expect(heading).toHaveTextContent("Welcome to Your Next.js Starter Template");
+    expect(heading).toBeInTheDocument();
   });
 });
